9a4310e3b450db8265f8a0c91d73888abb8e8039
Javier Pilco <brandon.pilco3401@utc.edu.ec>
2025-06-29 17:36:18 -0500

    7 Configuracion de las urls del login

--- Art/urls.py
+++ Art/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('Aplicaciones.Seccion.urls')),  # Incluye las rutas de tu app
 ]

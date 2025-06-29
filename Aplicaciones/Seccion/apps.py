bfef0f367d99488cccfbcab68aff305a54089529
Javier Pilco <brandon.pilco3401@utc.edu.ec>
2025-06-29 16:58:38 -0500

    3 Configuracion de los ajustes

--- Aplicaciones/Seccion/apps.py
+++ Aplicaciones/Seccion/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SeccionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Seccion'
+    name = 'Aplicaciones.Seccion'
